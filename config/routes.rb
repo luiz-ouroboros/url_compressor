df6870c94e1635680c93219b6a0026f43a19817f
Rufi Dev <luiz@ouroboros.dev.br>
2025-03-08 22:45:01 -0300

    Fix priority routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :redirections, only: %i[create index]
+
   get '/:target_key', to: 'redirections#show'
   delete '/:secret_key', to: 'redirections#destroy'
   get '/:secret_key/history', to: 'redirections#history'
-
-  resources :redirections, only: %i[create index]
 end
